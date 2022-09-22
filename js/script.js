c9424fdf936567060d14d46ca0efd01dc71c5d36
seaho5599 <seaho5599@gmail.com>
2022-09-22 16:23:41 +0900

    모달창 업로드

--- js/script.js
+++ js/script.js
@@ -2,16 +2,35 @@
 
 // jquery 를 이용(html, css, js)
 $(document).ready(function () {
+  $("html").css("overflow", "hiden");
+  let modalWrap = $(".modal-wrap");
+  let modalClose = $(".modal-close");
+  modalClose.click(function () {
+    modalWrap.stop().fadeOut(100);
+    // 추가기능:  스크롤바 살리기
+    $("html").css("overflow", "auto");
+  });
+  let modalMain = $(".modal-main");
+  // 내용배경 클릭
+  modalMain.click(function (event) {
+    event.stopPropagation();
+  });
+  // 전체 배경 클릭
+  modalWrap.click(function () {
+    modalWrap.stop().fadeOut(100);
+    $("html").css("overflow", "auto");
+  });
+
   //전체메뉴 보기 기능
   //.more-wrap 저장해서 활용하자.
-  let more_wrap = $('.more-wrap');
+  let more_wrap = $(".more-wrap");
   // .member-more 저장해서 활용하자.
-  let member_more = $('.member-more');
+  let member_more = $(".member-more");
   member_more.click(function () {
     more_wrap.fadeIn(300);
   });
   //,more-div-close를 활용하자.
-  let more_div_close = $('.more-div-close');
+  let more_div_close = $(".more-div-close");
   more_div_close.click(function () {
     more_wrap.fadeOut(300);
   });
@@ -21,19 +40,26 @@ $(document).ready(function () {
     more_wrap.fadeOut(300);
   });
 
-  $('.more-div').click(function(event){
+  $(".more-div").click(function (event) {
     event.stopImmediatePropagation();
   });
 
-
   // 모바일 메뉴 기능
   // .mb-bt 를 저장해서 활용하자.
-  $('.mb-bt').click(function (e) {
+  $(".mb-bt").click(function (e) {
     e.preventDefault();
-    $(this).toggleClass('mb-bt-open');
-    $('.mb-dim').toggleClass('mb-dim-open');
-    $('.mb-wrap').toggleClass('mb-wrap-open');
+    $(this).toggleClass("mb-bt-open");
+    $(".mb-dim").toggleClass("mb-dim-open");
+    $(".mb-wrap").toggleClass("mb-wrap-open");
   });
+  //모바일 버튼 기능 초기화
+  function buttonReset() {
+    $(".mb-bt").removeClass("mb-bt-open");
+    $(".mb-dim").removeClass("mb-dim-open");
+    $(".mb-wrap").removeClass("mb-wrap-open");
+    $(".mb-menu>li").height(60);
+    $(".mb-mainmenu").removeClass("mb-mainmenu-open");
+  }
 
   // 화면 사이즈 체크
   $(window).resize(function () {
@@ -42,70 +68,122 @@ $(document).ready(function () {
     // 1000 px 보다 크면
     if (temp > 1000) {
       // 모바일 버튼 기능 초기화
-      $('.mb-bt').removeClass('mb-bt-open');
-      $('.mb-dim').removeClass('mb-dim-open');
-      $('.mb-wrap').removeClass('mb-wrap-open');
-      $('.mb-menu>li').height(60)
-      $('.mb-mainmenu').removeClass('mb-mainmenu-open');
+      buttonReset();
     }
-
   });
 
   // 모바일 메뉴 펼치기 기능
   // 1. 모바일 메뉴 저장
-  let mb_mainmenu = $('.mb-mainmenu');
+  let mb_mainmenu = $(".mb-mainmenu");
   // 2. 모바일 서브메뉴 저장
-  let mb_submenu = $('.mb-submenu');
+  let mb_submenu = $(".mb-submenu");
   // 3. 펼쳐질 서브메뉴의 높이값
   let mb_submenu_high = [];
   // 높이값 계산을 실행
   $.each(mb_submenu, function (index) {
     // 각각의 .mb-submenu 로 가서
     // li 의 개수를 파악한다.
-    let count = $(this).find('li').length;
+    let count = $(this).find("li").length;
     // 최종 결과를 저장(56*count)
-    mb_submenu_high[index] = (56 * count);
+    mb_submenu_high[index] = 56 * count;
   });
 
   // 최종 결과
-  let mb_li = $('.mb-menu > li');
+  let mb_li = $(".mb-menu > li");
   $.each(mb_mainmenu, function (index) {
     $(this).click(function (e) {
       //  웹브라우저 갱신 막기
       e.preventDefault();
 
       // mb-mainmenu를 toggleClass 한다.
-      $(this).toggleClass('mb-mainmenu-open');
+      $(this).toggleClass("mb-mainmenu-open");
       //만약에 mb-mainmenu-open이 있으면 펼치고
       //없으면 닫고
-      let active = $(this).hasClass('mb-mainmenu-open');
+      let active = $(this).hasClass("mb-mainmenu-open");
       if (active) {
         let temp = mb_submenu_high[index];
         mb_li.eq(index).height(temp + 60);
       } else {
         mb_li.eq(index).height(60);
       }
-
     });
   });
   //모바일 메뉴배경 클릭시 사라짐.
-  let mb_dim = $('.mb-dim');
+  let mb_dim = $(".mb-dim");
   mb_dim.click(function () {
     //모바일 버튼 기능 초기화
-    $('.mb-bt').removeClass('mb-bt-open');
-    $('.mb-dim').removeClass('mb-dim-open');
-    $('.mb-wrap').removeClass('mb-wrap-open');
-    $('.mb-menu>li').height(60)
-    $('.mb-mainmenu').removeClass('mb-mainmenu-open');
-  })
-});
+    buttonReset();
+  });
 
+  //커뮤니티 영역 데이터 연동
+  //이룸소식        : Array[] 구현
+  let infoLinkArr = ["#1", "#2", "#3", "#4"];
+  let infoTitleArr = [
+    "7월 취창업 특강 지원자 모집 ♡",
+    "6월 문화특강 [모스큐브&멘톨비누 만들기]",
+    "6월 인문학특강 [4차 산업혁명과 청년]",
+    "< 내꿈공간(내 일을 꿈꾸는 청년창업공간) 대관 안내 >",
+  ];
+  let infoDateArr = ["2021.06.27", "2021.06.13", "2021.06.08", "2021.06.02"];
+  let communityList = $(".data-info");
+  let commnuityOutput = "";
+  for (let i = 0; i < infoLinkArr.length; i++) {
+    let temp = `
+      <li>
+      <a href="${infoLinkArr[i]}">${infoTitleArr[i]}</a>
+      <span>${infoDateArr[i]}</span>
+      </li>
+    `;
+
+    commnuityOutput += temp;
+  }
+  communityList.html(commnuityOutput);
+
+  // 청년정책 새소식  : 객체  {} 구현
+  let newsData = [
+    {
+      link: "#1",
+      title: "2022 경남 청년 라이브커머스 아카데미 참가자 모집 공고",
+      date: "2022.06.27",
+    },
+    {
+      link: "#2",
+      title: "청춘다락, 7월 프로그램 참여자 선정 결과(예비명단 포함)",
+      date: "2022.06.23",
+    },
+    {
+      link: "#3",
+      title: "「2022년 청년, 거제에서 한 달 살아보기」 참여 청년 모집!!",
+      date: "2022.06.15",
+    },
+    {
+      link: "#4",
+      title: "2022년 거제시 청년 월세 지원사업 선정 결과",
+      date: "2022.06.08",
+    },
+  ];
+
+  let dataNewsDiv = $(".data-news");
+  let dataNewsOutput = "";
+  for (let i = 0; i < newsData.length; i++) {
+    //데이터를 한개씩 가져와서 뜯는다.
+    let data = newsData[i];
+    let temp = `
+      <li>
+      <a href="${data.link}">${data.title}</a>
+      <span>${data.date}</span>
+      </li>
+    `;
+
+    dataNewsOutput += temp;
+  }
+  dataNewsDiv.html(dataNewsOutput);
+});
 
 // js 를 이용(html, css, js,멀티미디어 요소)
 window.onload = function () {
-
   // 비주얼 슬라이드
-  let sw_visual = new Swiper('.sw-visual', {
+  let sw_visual = new Swiper(".sw-visual", {
     loop: true,
     speed: 1000,
     effect: "fade",
@@ -120,35 +198,32 @@ window.onload = function () {
   });
   // 일시멈춤 버튼
   // 현재 상태 저장
-  let slide_now = 'ing';
+  let slide_now = "ing";
 
-  $('.sw-visual-bt').click(function () {
-
-    if (slide_now == 'ing') {
+  $(".sw-visual-bt").click(function () {
+    if (slide_now == "ing") {
       // 현재 슬라이드 진행 중이라면
       // 슬라이드를 멈춘다.
-      slide_now = 'stop';
+      slide_now = "stop";
       sw_visual.autoplay.stop();
-      $(this).find('span').text('play_arrow');
-
+      $(this).find("span").text("play_arrow");
     } else {
       // 현재 슬라이드가 멈췄다.
       // 다시 슬라이드를 진행한다.
-      slide_now = 'ing';
+      slide_now = "ing";
       sw_visual.autoplay.start();
-      $(this).find('span').text('pause');
+      $(this).find("span").text("pause");
     }
-
   });
 
   // 배너 슬라이드
-  let sw_banner = new Swiper('.sw-banner', {
+  let sw_banner = new Swiper(".sw-banner", {
     loop: true,
     slidesPerView: 3,
     spaceBetween: 20,
     navigation: {
-      prevEl: '.sw-banner-prev',
-      nextEl: '.sw-banner-next'
+      prevEl: ".sw-banner-prev",
+      nextEl: ".sw-banner-next",
     },
     autoplay: {
       delay: 1000,
@@ -156,40 +231,106 @@ window.onload = function () {
     },
     breakpoints: {
       1400: {
-        slidesPerView: 6
+        slidesPerView: 6,
       },
       1260: {
-        slidesPerView: 5
+        slidesPerView: 5,
       },
       1000: {
-        slidesPerView: 4
+        slidesPerView: 4,
       },
       860: {
-        slidesPerView: 3
-      }
-    }
+        slidesPerView: 3,
+      },
+    },
   });
 
   // 배너 슬라이드 일시멈춤 버튼
-  $('.sw-banner-pause').click(function () {
+  $(".sw-banner-pause").click(function () {
     // 현재 span 태그 안쪽의 글자를 읽는다.
-    let temp = $(this).find('span').text();
-    if (temp == 'play_arrow') {
-      $(this).find('span').text('pause');
+    let temp = $(this).find("span").text();
+    if (temp == "play_arrow") {
+      $(this).find("span").text("pause");
       sw_banner.autoplay.stop();
     } else {
-      $(this).find('span').text('play_arrow');
+      $(this).find("span").text("play_arrow");
       sw_banner.autoplay.start();
     }
   });
 
   // 화면 위로 이동
-  $('.gotop').click(function () {
+  //goTop('클레스명')
+  function goTop(_el, _speed) {
+    $(_el).click(function (event) {
+      //a태그인 경우 href를 제어한다.(웹브라우저 갱신)
+      event.preventDefault();
+      $("html").animate(
+        {
+          scrollTop: 0,
+        },
+        _speed
+      );
+    });
+  }
+  goTop(".gotop", 1000);
 
-    $('html').animate({
-      scrollTop: 0
-    }, 1000);
+  // $('.gotop').click(function () {
 
-  });
+  //   $('html').animate({
+  //     scrollTop: 0
+  //   }, 1000);
 
-}
+  // });
+
+  //활동 갤러리 함수 작성
+  //자료를 전달하면 활동 갤러리 목록에 출력을 한다.(링크, 사진, 날짜, 제목)
+  //특정한 html 의 요소에 출력을 해야한다.
+  //출력할 데이터 모음
+  let galleryData = [
+    {
+      link: "#",
+      title: "3월 인문학 특강 [우리 삶에 철학이 필요한 이유]",
+      date: "2022-04-04",
+      pic: "gallery1.jpg",
+    },
+    {
+      link: "#",
+      title: "12월 문화특강 주간 ♡ 크리스마스 입욕제 만들기",
+      date: "2021-12-14",
+      pic: "gallery2.jpg",
+    },
+    {
+      link: "#",
+      title: "12월 문화특강 주간 ♡ 플레이팅 도마 만들기",
+      date: "2021-12-14",
+      pic: "gallery3.jpg",
+    },
+  ];
+
+  function showGalleryInfo(_tag, _data) {
+    //대상을 찾아라.
+    let who = $(_tag);
+    who.attr("href", _data.link); // attribute
+    let html = `
+  <span class="gallery-img"></span>
+    <p class="gallery-cont">
+    <span class="gallery-title">
+      ${_data.title}
+    </span>
+    <span class="gallery-date">
+      ${_data.date}
+    </span>
+    </p>
+  `;
+
+    //출력을 하자.
+    who.html(html);
+    // 배경을 넣자.
+    let bg = who.find(".gallery-img");
+    bg.css("background", "url(images/" + _data.pic + ") no-repeat center");
+    bg.css("background-size", "cover");
+  }
+  showGalleryInfo("#gallery-list-1", galleryData[0]);
+  showGalleryInfo("#gallery-list-2", galleryData[1]);
+  showGalleryInfo("#gallery-list-3", galleryData[2]);
+};
